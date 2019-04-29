9bcde305f58e1080e06ad54fa18903d7f80d53cb
Andrew Gorham <andrew.gorham@mymail.champlain.edu>
2019-04-28 21:16:52 -0400

    Finish requirements

--- UI.py
+++ UI.py
@@ -12,23 +12,8 @@ Choose:
 game_instance = nim.Game()  # Creates a new game instance for play
 
 
-def print_rules():
-    """
-    Prints out the game rules in paragraph form.
-    :return: rules string
-    """
-
-    rules = """
-    There are a set amount of balls on the board (12).
-    You and the computer take turns dropping the balls from
-    the upper ball area, called the pool, to the lower ball area, called the dump.
-    You can drop 1, 2 or 3 balls at a time. The one who drops the final ball is the winner
-    """
-    print(rules)
-
-
 menu_dict = {'1': game_instance.human_turn,
-             '2': print_rules,
+             '2': game_instance.print_rules,
              '3': sys.exit}
 
 
--- nim.py
+++ nim.py
@@ -17,80 +17,118 @@ import sys
 
 
 class Game:
+    """
+    Class where any gameplay calculation and representation happens
+    """
     def __init__(self):
-        self.balls = 24
+        """
+        Initializes game class
+        """
+        self._balls = 24
         self.total_balls = 24
 
     def __str__(self):
-        print(f'There are {self.ball_count()} balls in the Pool Pile')
-        print(f'There are {self.total_balls - self.ball_count()} balls in the Drop Pile')
+        """
+        String representation of the game's current state
+        :return: Strings describing game
+        """
+        print(f'There are {self.balls} balls in the Pool Pile')
+        print(f'There are {self.total_balls - self.balls} balls in the Drop Pile')
 
-    def ball_count(self):
-        return self.balls
+    @property
+    def balls(self):
+        """
+        Getter for balls property
+        :return: balls
+        """
+        return self._balls
+
+    @balls.setter
+    def balls(self, amt):
+        """
+        Action of dropping balls using setter
+        :param amt: the amount of balls being dropped
+        """
+        self._balls = self._balls - amt
 
     def game_state_ascii(self):
-        pool_pile = ''
-        drop_pile = ''
-        for i in range(self.ball_count()):
-            pool_pile = pool_pile + 'O'
-        for i in range(self.total_balls - self.ball_count()):
-            drop_pile = drop_pile + 'O'
+        """
+        The ASCII representation of the game's current state
+        :return: ASCII of Game
+        """
+        pool_pile = ''.join(['O' * i for i in range(self.balls)][-1:])
+        drop_pile = ''.join(['O' * i for i in range(self.total_balls - self.balls)][-1:])
         print(f'Pool Pile: {pool_pile}')
         print(f'Drop Pile: {drop_pile}')
         print('`````````````````````````')
 
-    def ball_drop(self, amt):
-        self.balls = self.balls - int(amt)
-
     def human_turn(self):
+        """
+        The behavior of the human on their turn in the game
+        :return: drop correct number of balls and print the action
+        """
         self.game_state_ascii()
         choice = input('How many balls do you want to drop? (1, 2, or 3)')
         if choice == '1':
-            self.ball_drop(1)
-            if self.ball_count() == 0:
+            self.balls = 1
+            if self.balls == 0:
                 print('Congrats, you win!')
                 sys.exit()
             self.cpu_turn()
         elif choice == '2':
-            self.ball_drop(2)
-            if self.ball_count() == 0:
+            self.balls = 2
+            if self.balls == 0:
                 print('Congrats, you win!')
                 sys.exit()
             self.cpu_turn()
         elif choice == '3':
-            self.ball_drop(3)
-            if self.ball_count() == 0:
+            self.balls = 3
+            if self.balls == 0:
                 print('Congrats, you win!')
                 sys.exit()
             self.cpu_turn()
 
     def cpu_turn(self):
         """
-        The behavior of the cpu on its turn in the standard game
+        The behavior of the cpu on its turn in the game
         :return: drop correct number of balls and print the action
         """
-        if self.ball_count() % 4 == 3:
-            self.ball_drop(3)
+        if self.balls % 4 == 3:
+            self.balls = 3
             print('The CPU has dropped 3 balls')
-            if self.ball_count() == 0:
+            if self.balls == 0:
                 print('The computer has won')
                 sys.exit()
             else:
                 self.human_turn()
-        elif self.ball_count() % 4 == 2:
-            self.ball_drop(2)
+        elif self.balls % 4 == 2:
+            self.balls = 2
             print('The CPU has dropped 2 balls')
-            if self.ball_count() == 0:
+            if self.balls == 0:
                 print('The computer has won')
                 sys.exit()
             else:
                 self.human_turn()
-        elif self.ball_count() % 4 == 1:
-            self.ball_drop(1)
+        elif self.balls % 4 == 1:
+            self.balls = 1
             print('The CPU has dropped 1 ball')
-            if self.ball_count() == 0:
+            if self.balls == 0:
                 print('The computer has won')
                 sys.exit()
             else:
                 self.human_turn()
 
+    @staticmethod
+    def print_rules():
+        """
+        Prints out the game rules in paragraph form.
+        :return: rules string
+        """
+
+        rules = """
+        There are a set amount of balls on the board (12).
+        You and the computer take turns dropping the balls from
+        the upper ball area, called the pool, to the lower ball area, called the dump.
+        You can drop 1, 2 or 3 balls at a time. The one who drops the final ball is the winner
+        """
+        print(rules)
